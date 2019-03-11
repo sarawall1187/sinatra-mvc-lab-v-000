cefa7a19581904db8fa818b2df9d152607be5f7a
Sara Walker <sarawall1187@gmail.com>
2019-03-11 17:50:39 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ class App < Sinatra::Base
     erb :user_input
   end
 
-  post '/' do
+  post '/piglatinize' do
     @translated_text = PigLatinizer.new(params[:user_text])
     erb :translation
   end
