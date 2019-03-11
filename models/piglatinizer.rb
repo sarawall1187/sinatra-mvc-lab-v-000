12e1fd51e8e7672c9d36ae827ce7270a8ebd7a60
Sara Walker <sarawall1187@gmail.com>
2019-03-11 18:10:50 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -9,7 +9,7 @@ class PigLatinizer
    words.each_with_index do |word, i|
    translation = ''
    qu = false
-   if vowels.include? word[0]
+   if vowels.include? word[0].downcase
        translation = word + 'ay'
        result.push(translation)
     # elsif word == "i" || word == "I"
