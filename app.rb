40af1ba003cd112227e82bb6d63f49afba1bec3b
Sara Walker <sarawall1187@gmail.com>
2019-03-11 18:22:23 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,8 +7,8 @@ class App < Sinatra::Base
   end
 
   post '/piglatinize' do
-    @piglatinizer = PigLatinizer.new(params[:user_phrase])
-    @text = @piglatinizer.piglatinize
+    @piglatinizer = PigLatinizer.new
+    @text = @piglatinizer.piglatinize(params[:user_phrase])
     erb :piglatinize
   end
 end
