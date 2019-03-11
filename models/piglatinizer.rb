5ea230667be305bd70014fd069c820b91956ca33
Sara Walker <sarawall1187@gmail.com>
2019-03-11 18:05:56 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -14,7 +14,7 @@ class PigLatinizer
        result.push(translation)
     elsif word == "i" || word == "I"
       translation = word + 'way'
-      
+
    else
        word = word.split('')
        count = 0
